8b344d070fcad328697edf998ce0f4ef1d23da03
Corey Norris <coreypnorris@gmail.com>
2014-03-30 16:38:15 -0700

    finished intro to rails homework

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,8 +4,52 @@ class ContactsController < ApplicationController
     render('contacts/index.html.erb')
   end
 
+  def new
+    @contact = Contact.new
+    render('contacts/new.html.erb')
+  end
+
+  def create
+    @contact = Contact.new(:name => params[:name],
+                           :phone => params[:phone],
+                           :email => params[:email])
+    if @contact.save
+      render('contacts/success.html.erb')
+    else
+      render('contacts/new.html.erb')
+    end
+  end
+
   def show
     @contact = Contact.find(params[:id])
     render('contacts/show.html.erb')
   end
+
+  def edit
+    @contact = Contact.find(params[:id])
+    render('contacts/edit.html.erb')
+  end
+
+  def update
+    @contact = Contact.find(params[:id])
+    if @contact.update(:name => params[:name],
+                       :email => params[:email],
+                       :phone => params[:phone])
+      render('contacts/success.html.erb')
+    else
+      render('contacts/edit.html.erb')
+    end
+  end
+
+  def destroy
+    @contact = Contact.find(params[:id])
+    @contact.destroy
+    render('contacts/destroy.html.erb')
+  end
 end
+
+
+
+
+
+
